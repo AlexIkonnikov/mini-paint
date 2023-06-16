94bf8741d603775447aa0e5f6b5fbfded58eb8b4
Aleksandr <ikonnikovAI@lad24.ru>
2023-06-16 23:06:05 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { ToolBar } from './widgets/ToolBar'
+import './index.css'
+
+function App() {
+  return (
+    <ToolBar/>
+  )
+}
+
+export default App
---
+++ src/widgets/ToolBar/ToolBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const ToolBar = () => {
+  return (
+    <div className='toolbar'>
+
+    </div>
+  )
+}
+
+export default ToolBar
