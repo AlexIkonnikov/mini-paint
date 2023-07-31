2a7dface1dac7e02bf5e6bdfcf99e6f9680fa66a
Aleksandr <ikonnikovAI@lad24.ru>
2023-07-31 21:08:38 +0600

    feat: add simple routing

--- index.tsx
+++ index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import App from './src/App';
 
+const createPath = () => {
+  const today = Number(new Date());
+  return today.toString(16);
+};
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:id" element={<App />} />
+        <Route path="*" element={<Navigate to={createPath()} replace />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
