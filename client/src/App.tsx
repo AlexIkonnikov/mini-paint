c080497cf7467550abedcbeefe9914e68fa76d50
Aleksandr <ikonnikovAI@lad24.ru>
2023-08-01 21:27:33 +0600

    chore: remove experemental code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,6 @@ import { Canvas } from './widgets/Canvas';
 import './index.css';
 
 function App() {
-  console.log('render APPP');
-  new WebSocket('ws://localhost:8000/');
   return (
     <div className="app">
       <ToolBar />
