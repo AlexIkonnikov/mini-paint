c6b5c1f6f3c1463f9afefb0fa7f26d35afcfd413
Aleksandr <ikonnikovAI@lad24.ru>
2023-08-03 21:42:05 +0600

    refactor: refactor userList component

--- client/src/entities/user/ui/UserList.tsx
+++ client/src/entities/user/ui/UserList.tsx
@@ -1,20 +1,22 @@
+import { Avatar, Row, Tooltip } from 'antd';
 import { observer } from 'mobx-react-lite';
 import { FC } from 'react';
 
 import UserStore from '../model/UserStore';
 
-import UserAvatar from './UserAvatar';
-
 const UserList: FC = observer(() => {
   const { users } = UserStore;
 
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+    <Row>
       {users.map(props => (
-        <UserAvatar {...props} key={props.id} />
+        <Tooltip key={props.id} title={props.name}>
+          <Avatar style={{ marginRight: 5 }} size={'large'}>
+            {props.name[0]}
+          </Avatar>
+        </Tooltip>
       ))}
-    </div>
+    </Row>
   );
 });
 
