f2e50e5547c7237c730612ec69c404bc4c4a0b74
Aleksandr <ikonnikovAI@lad24.ru>
2023-08-05 18:02:42 +0600

    debug: add socket connection after auth

--- client/src/features/auth/ui/AuthFormModal.tsx
+++ client/src/features/auth/ui/AuthFormModal.tsx
@@ -1,27 +1,48 @@
 import { Input, Button, Form, Modal } from 'antd';
 import { observer } from 'mobx-react-lite';
+import { useParams } from 'react-router-dom';
 
 import { UserStore } from '../../../entities/user';
 
 const AuthFormModal = observer(() => {
   const { currentUser } = UserStore;
+  const params = useParams();
 
   const onChange = ({ username }: { username: string }) => {
     UserStore.setCurrentUser({
-      id: username + 'id',
+      id: username + '-' + new Date().getTime(),
       name: username,
     });
+
+    const ws = new WebSocket('ws://localhost:8000/');
+
+    const data = JSON.stringify({
+      socket: params.id,
+      type: 'hello',
+      ...UserStore.currentUser,
+    });
+
+    ws.onopen = () => {
+      ws.send(data);
+    };
+
+    ws.onmessage = ({ data }) => {
+      const { type, payload } = JSON.parse(data);
+      if (type === 'hello') {
+        UserStore.addUser(payload);
+      }
+    };
   };
 
   return (
     <Modal
       open={currentUser === null}
-      title={'Авторизация:'}
+      title={'Представьтесь:'}
       closable={false}
       footer={[
         <Form.Item key="submit">
           <Button type="primary" form={'auth_from'} htmlType="submit">
-            Авторизоваться
+            Войти
           </Button>
         </Form.Item>,
       ]}>
