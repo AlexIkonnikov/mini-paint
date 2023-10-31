fb2146c76e5dcd3acbb119819c7d0564ff55cb1d
Aleksandr <ikonnikovAI@lad24.ru>
2023-10-31 20:00:49 +0600

    debug(drawing): create Line class and rewrite LineDrawingStrategy

--- client/src/features/drawing/lib/LineDrawingStrategy.ts
+++ client/src/features/drawing/lib/LineDrawingStrategy.ts
@@ -1,13 +1,14 @@
+import { Line, ShapeStorage } from '../../../entities/shapes';
 import { Canvas } from '../../../shared/lib/Canvas';
 import { IDrawerStrategy } from '../../../shared/lib/DrawerContext';
 
 class LineDrawingStrategy implements IDrawerStrategy {
-  x = 0;
-  y = 0;
   ctx: CanvasRenderingContext2D;
+  line: Line;
 
   constructor() {
     this.ctx = Canvas.getInstance().ctx;
+    this.line = new Line(0, 0);
   }
 
   get name(): string {
@@ -15,17 +16,28 @@ class LineDrawingStrategy implements IDrawerStrategy {
   }
 
   beforeDraw(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+    this.line.x = x;
+    this.line.y = y;
   }
 
   draw(newX: number, newY: number): void {
-    const { x, y } = this;
+    const { width, height } = this.ctx.canvas;
+    this.ctx.clearRect(0, 0, width, height);
+    ShapeStorage.restore();
 
-    this.ctx.beginPath();
-    this.ctx.moveTo(x, y);
-    this.ctx.lineTo(newX, newY);
-    this.ctx.stroke();
+    this.line.setEndPoint([newX, newY]);
+    this.line.draw();
+  }
+
+  afterDraw() {
+    if (this.line.isVisible()) {
+      return;
+    }
+
+    this.line.strokeStyle = this.ctx.strokeStyle;
+    this.line.lineWidth = this.ctx.lineWidth;
+    ShapeStorage.addShape(this.line);
+    this.line = new Line(0, 0);
   }
 }
 
